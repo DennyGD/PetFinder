ff6f44c15cd1bb25ae5dddaae2252360e9205e0f
DennyGD <denny.dimitrova@abv.bg>
2016-02-18 19:08:32 +0200

    Kendo scaffolded Users controller in Administration
    Introduced Kendo to project
    Created Admin Area

--- Source/Web/PetFinder.Web/App_Start/BundleConfig.cs
+++ Source/Web/PetFinder.Web/App_Start/BundleConfig.cs
@@ -16,11 +16,13 @@
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include("~/Scripts/KendoUi/kendo.web.min.js", "~/Scripts/KendoUi/kendo.aspnetmvc.min.js"));
         }
 
         private static void RegisterStyles(BundleCollection bundles)
         {
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/kendo").Include("~/Content/KendoUi/kendo.common.min.css", "~/Content/KendoUi/kendo.metro.min.css"));
         }
     }
 }
