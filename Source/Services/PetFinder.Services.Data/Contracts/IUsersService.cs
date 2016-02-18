91527b080801396f9c5f71509b9273d530850ed5
DennyGD <denny.dimitrova@abv.bg>
2016-02-18 19:37:15 +0200

    Introduces UserService

---
+++ Source/Services/PetFinder.Services.Data/Contracts/IUsersService.cs
@@ -0,0 +1,11 @@
+﻿namespace PetFinder.Services.Data.Contracts
+{
+    using System.Linq;
+
+    using PetFinder.Data.Models;
+
+    public interface IUsersService
+    {
+        IQueryable<User> All(bool includeDeleted);
+    }
+}
---
+++ Source/Services/PetFinder.Services.Data/UsersService.cs
@@ -0,0 +1,30 @@
+﻿namespace PetFinder.Services.Data
+{
+    using System.Linq;
+
+    using Contracts;
+    using PetFinder.Data.Models;
+    using PetFinder.Data.Common;
+
+    public class UsersService : IUsersService
+    {
+        private readonly IDbRepository<User> usersRepo;
+
+        public UsersService(IDbRepository<User> usersRepo)
+        {
+            this.usersRepo = usersRepo;
+        }
+
+        public IQueryable<User> All(bool includeDeleted)
+        {
+            if (includeDeleted)
+            {
+                return this.usersRepo.AllWithDeleted();
+            }
+            else
+            {
+                return this.usersRepo.All();
+            }
+        }
+    }
+}
