dd8a37973e762803780f7c4eb7e8807bff5ce280
DennyGD <denny.dimitrova@abv.bg>
2016-02-23 01:20:05 +0200

    Added base controller for the private area

---
+++ Source/Web/PetFinder.Web/Areas/Private/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿namespace PetFinder.Web.Areas.Private.Controllers
+{
+    using Services.Data.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class UsersController : BasePrivateController
+    {
+        public UsersController(IUsersService usersService)
+            : base(usersService)
+        {
+        }
+
+        public ActionResult MyProfile()
+        {
+            var fullName = this.CurrentUser.FirstName + " " + this.CurrentUser.LastName;
+            this.ViewBag.Name = fullName;
+            return this.View();
+        }
+    }
+}
