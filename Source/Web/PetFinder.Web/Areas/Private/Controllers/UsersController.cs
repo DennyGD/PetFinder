13729007f0fa747ea361474cd165416c165ed8e0
DennyGD <denny.dimitrova@abv.bg>
2016-02-23 12:42:52 +0200

    Basic functionality for private area completed

--- Source/Web/PetFinder.Web/Areas/Private/Controllers/UsersController.cs
+++ Source/Web/PetFinder.Web/Areas/Private/Controllers/UsersController.cs
@@ -1,12 +1,13 @@
 ﻿namespace PetFinder.Web.Areas.Private.Controllers
 {
+    using System.Linq;
     using System.Web.Mvc;
 
+    using Infrastructure.Mapping;
     using Services.Data.Contracts;
     using ViewModels;
-    using System.Linq;
     using Web.ViewModels.Posts;
-    using Infrastructure.Mapping;
+
     public class UsersController : BasePrivateController
     {
         public UsersController(IUsersService usersService)
@@ -28,6 +29,19 @@
                 .Posts
                 .AsQueryable()
                 .To<PostBaseViewModel>()
+                .ToList();
+
+            return this.View(data);
+        }
+
+        [HttpGet]
+        public ActionResult MyComments()
+        {
+            var data = this.CurrentUser
+                .Comments
+                .AsQueryable()
+                .OrderByDescending(x => x.CreatedOn)
+                .To<CommentPrivateViewModel>()
                 .ToList();
 
             return this.View(data);
---
+++ Source/Web/PetFinder.Web/Areas/Private/ViewModels/CommentPrivateViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace PetFinder.Web.Areas.Private.ViewModels
+{
+    using System;
+
+    using AutoMapper;
+
+    using Data.Models;
+    using Infrastructure.Mapping;
+
+    public class CommentPrivateViewModel : IMapFrom<Comment>, IHaveCustomMappings
+    {
+        public string Content { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int PostId { get; set; }
+
+        public bool PostIsDeleted { get; set; }
+
+        public void CreateMappings(IMapperConfiguration configuration)
+        {
+            configuration.CreateMap<Comment, CommentPrivateViewModel>()
+                .ForMember(
+                x => x.PostIsDeleted,
+                opts => opts.MapFrom(x => x.Post.IsDeleted));
+        }
+    }
+}
