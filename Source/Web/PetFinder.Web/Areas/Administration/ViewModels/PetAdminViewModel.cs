7e32950b0df147b566e1d3ab2a479ab1c6c91ec1
DennyGD <denny.dimitrova@abv.bg>
2016-02-24 11:11:05 +0200

    Added missing client validation to admin panel

--- Source/Web/PetFinder.Web/Areas/Administration/ViewModels/PetAdminViewModel.cs
+++ Source/Web/PetFinder.Web/Areas/Administration/ViewModels/PetAdminViewModel.cs
@@ -1,12 +1,17 @@
 ﻿namespace PetFinder.Web.Areas.Administration.ViewModels
 {
+    using System.ComponentModel.DataAnnotations;
+
+    using Common.Constants;
     using Data.Models;
     using Infrastructure.Mapping;
-
+    
     public class PetAdminViewModel : BaseAdminViewModel, IMapFrom<Pet>
     {
         public int Id { get; set; }
 
+        [Required]
+        [MaxLength(Models.PetNameMaxLength)]
         public string Name { get; set; }
     }
 }
